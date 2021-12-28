76640f2f2a010296ab6c9044161844549ba34a31
Heang Ly <heanglypa@gmail.com>
2021-12-27 22:43:09 -0500

    styled component

---
+++ styled-component/hubble/src/components/Header.js
@@ -0,0 +1,12 @@
+import { StyledHeader } from './styles/Header.styled'
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>Hubble</h1>
+      <div className='test'>ok</div>
+    </StyledHeader>
+  )
+}
+
+export default Header
